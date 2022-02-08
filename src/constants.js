920d86c79d138080d9a7857d1e06eef363749d2a
Hambur0703 <hamburhuang@gmail.com>
2022-02-07 19:58:05 -0500

    GoAround webapp part

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjgxMzYwNzUsInVzZXJuYW1lIjoiaGFtYnVyIn0.VPENHxVA-XLgMBlYP1oYsgiKguSW_a0Qmo9Bq_4sVbQ';
+
+export const BASE_URL = 'https://calm-shadow-318121.uc.r.appspot.com';
+
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
